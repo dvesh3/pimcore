9a4db77448f2435901e5c966f9a076df4165959f
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-10 18:44:12 +0100

    tests

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -8,7 +8,5 @@ cp -r .github/ci/files/app app
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/web web
 
-cp -rf .github/ci/files/default.conf ./etc/nginx/conf.d/default.conf
-
 cp .github/ci/files/extensions.template.php var/config/extensions.php
 cp app/config/parameters.example.yml app/config/parameters.yml
