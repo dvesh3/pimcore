2b2f10e2b403b46b87a099ef7dd8cd1a8136bae4
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-12 11:47:23 +0100

    GitHub Actions for 6.x branch

---
+++ .github/ci/files/setup-fpm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Setting up FPM ..."
+
+sudo cp -f .github/files/apache/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
--- .github/ci/scripts/setup-apache.sh
+++ .github/ci/scripts/setup-apache.sh
@@ -3,27 +3,24 @@
 set -e
 
 # set home directory permissions to be readable by apache
-sudo chmod 0755 $PIMCORE_PROJECT_ROOT
+sudo chmod 0755 $(pwd)
 
 # install apache
 sudo apt-get update --allow-unauthenticated
-sudo apt-get install apache2
+sudo apt-get install apache2 libapache2-mod-fastcgi
+sudo a2enmod rewrite actions fastcgi alias env
+sudo rm -f /etc/apache2/sites-available/*
+sudo rm -f /etc/apache2/sites-enabled/*
 
-wget https://mirrors.edge.kernel.org/ubuntu/pool/multiverse/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
-sudo dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
+ # set up web server config
+.travis/setup-fpm.sh
 
-sudo mv /etc/apache2/ports.conf /etc/apache2/ports.conf.default
-echo "Listen 8010" | sudo tee /etc/apache2/ports.conf
+sudo cp -f .github/files/apache/apache-fpm.conf /etc/apache2/sites-available/pimcore-test.dev.conf
 
-sudo a2dissite 000-default
-
-sudo cp -f .github/ci/files/apache/apache-fpm.conf /etc/apache2/sites-available/pimcore-test.dev.conf
+# enable pimcore-test.dev config
+sudo ln -s /etc/apache2/sites-available/pimcore-test.dev.conf /etc/apache2/sites-enabled/pimcore-test.dev.conf
 
 VHOSTCFG=/etc/apache2/sites-available/pimcore-test.dev.conf
-
-echo "test pwd:"
-sudo journalctl | tail
-echo $(pwd)
 
 # configure apache virtual hosts - config was copied in the individual setup scripts above (FPM)
 sudo sed -e "s?%GITHUB_WORKSPACE_DIR%?$(pwd)?g" -i $VHOSTCFG
@@ -31,7 +28,4 @@ sudo sed -e "s?%PIMCORE_ENVIRONMENT%?$PIMCORE_ENVIRONMENT?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_DB_DSN%?$PIMCORE_TEST_DB_DSN?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_CACHE_REDIS_DATABASE%?$PIMCORE_TEST_CACHE_REDIS_DATABASE?g" -i $VHOSTCFG
 
-sudo a2ensite pimcore-test.dev
-
-sudo systemctl restart apache2
-cat systemctl status apache2.service
+sudo service apache2 restart
