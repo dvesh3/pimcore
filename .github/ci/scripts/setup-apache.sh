2f08866f8506e6d4b857760a0722ccf8fa9eef27
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-12 10:29:37 +0100

    GitHub Actions for 6.x branch

--- .github/ci/scripts/setup-apache.sh
+++ .github/ci/scripts/setup-apache.sh
@@ -12,7 +12,7 @@ sudo apt-get install apache2
 wget https://mirrors.edge.kernel.org/ubuntu/pool/multiverse/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
 sudo dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
 
-
+sudo journalctl | tail
 
 sudo rm -f /etc/apache2/sites-available/*
 sudo rm -f /etc/apache2/sites-enabled/*
@@ -25,6 +25,7 @@ sudo ln -s /etc/apache2/sites-available/pimcore-test.dev.conf /etc/apache2/sites
 VHOSTCFG=/etc/apache2/sites-available/pimcore-test.dev.conf
 
 echo "test pwd:"
+sudo journalctl | tail
 echo $(pwd)
 
 # configure apache virtual hosts - config was copied in the individual setup scripts above (FPM)
@@ -34,4 +35,3 @@ sudo sed -e "s?%PIMCORE_TEST_DB_DSN%?$PIMCORE_TEST_DB_DSN?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_CACHE_REDIS_DATABASE%?$PIMCORE_TEST_CACHE_REDIS_DATABASE?g" -i $VHOSTCFG
 
 sudo systemctl restart apache2
-sudo journalctl | tail
