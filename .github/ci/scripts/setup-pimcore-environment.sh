9c84e5e6f2b57015b5c87bdb5c1bdac9bef2081b
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-13 11:37:31 +0100

    GitHub Actions for 6.x branch

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -5,7 +5,7 @@ set -eu
 mkdir -p var/config
 mkdir -p var/logs
 
-sudo chmod 0755 var/logs
+sudo chmod 0777 var/logs
 
 cp -r .github/ci/files/app app
 cp -r .github/ci/files/bin/console bin/console
