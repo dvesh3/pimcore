5f8f5d47be7e6281ee1e72376822d1935ea5d492
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-13 10:49:01 +0100

    GitHub Actions for 6.x branch

--- .github/ci/scripts/setup-apache.sh
+++ .github/ci/scripts/setup-apache.sh
@@ -19,18 +19,14 @@ sudo a2enmod actions fcgid alias proxy_fcgi
 sudo mv /etc/apache2/ports.conf /etc/apache2/ports.conf.default
 echo "Listen 8080" | sudo tee /etc/apache2/ports.conf
 
-#cat /etc/apache2/ports.conf
+sudo cp -f .github/ci/files/apache/php-fpm.conf /etc/php/7.3/fpm/pool.d/www.conf
+sudo systemctl restart php7.3-fpm
 
-#sudo cp -f .github/ci/files/apache/php-fpm.conf /etc/php/7.3/fpm/pool.d/www.conf
-#sudo systemctl restart php7.3-fpm
-
-#sudo rm -f /etc/apache2/sites-available/*
-#sudo rm -f /etc/apache2/sites-enabled/*
+sudo rm -f /etc/apache2/sites-available/*
+sudo rm -f /etc/apache2/sites-enabled/*
 
 sudo cp -f .github/ci/files/apache/apache-fpm.conf /etc/apache2/sites-available/pimcore-test.dev.conf
-sudo a2dissite 000-default
-sudo a2ensite pimcore-test.dev
-#sudo ln -s /etc/apache2/sites-available/pimcore-test.dev.conf /etc/apache2/sites-enabled/pimcore-test.dev.conf
+sudo ln -s /etc/apache2/sites-available/pimcore-test.dev.conf /etc/apache2/sites-enabled/pimcore-test.dev.conf
 
 VHOSTCFG=/etc/apache2/sites-enabled/pimcore-test.dev.conf
 
@@ -40,5 +36,5 @@ sudo sed -e "s?%PIMCORE_ENVIRONMENT%?$PIMCORE_ENVIRONMENT?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_DB_DSN%?$PIMCORE_TEST_DB_DSN?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_CACHE_REDIS_DATABASE%?$PIMCORE_TEST_CACHE_REDIS_DATABASE?g" -i $VHOSTCFG
 sudo sed -e "s?%PIMCORE_TEST_PHP_VERSION%?$PIMCORE_TEST_PHP_VERSION?g" -i $VHOSTCFG
-
+echo "debug here"
 sudo systemctl restart apache2
