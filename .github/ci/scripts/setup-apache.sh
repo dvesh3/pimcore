77597de1af9aff7c682eb9c24a3563d2855fabe7
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-13 10:18:26 +0100

    GitHub Actions for 6.x branch

--- .github/ci/scripts/setup-apache.sh
+++ .github/ci/scripts/setup-apache.sh
@@ -8,9 +8,10 @@ sudo chmod -R 755 $(pwd)
 # install apache
 sudo apt-get update --allow-unauthenticated
 sudo apt-get install apache2  libapache2-mod-fcgid
-sudo a2enmod actions fcgid alias proxy_fcgi
+
 sudo apt-get install -y php7.3-fpm
-sudo service php7.3-fpm start
+sudo systemctl status php7.3-fpm
+sudo a2enmod actions fcgid alias proxy_fcgi
 
 sudo mv /etc/apache2/ports.conf /etc/apache2/ports.conf.default
 echo "Listen 8080" | sudo tee /etc/apache2/ports.conf
